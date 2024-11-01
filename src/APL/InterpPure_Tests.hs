9b0cbd13bbbd81f6dc397b63a9fe3bd63a3ac9d6
santiweide <ggy_778@outlook.com>
2024-11-01 11:28:04 +0100

    better comment for tuple

--- src/APL/InterpPure_Tests.hs
+++ src/APL/InterpPure_Tests.hs
@@ -35,24 +35,32 @@ tests =
         (Tuple [CstInt 1, CstInt 2])
         (ValTuple [ValInt 1, ValInt 2])
       -- tuple-eval-order is from left to right
+      -- (e1, e2, e3, e4)::e1->e2->e3->e4
       , evalTest
-        "tuple eval order"
+        "tuple eval order" 
         (Tuple [KvPut (CstInt 1) (CstInt 2), KvGet (CstInt 1), KvPut (CstInt 1) (CstInt 3), KvGet (CstInt 1)])
         (ValTuple [ValInt 2,ValInt 2,ValInt 3,ValInt 3])
-      -- 
+      -- (e0, e1, e2).2 = e2
       , evalTest
         "Projection valid tuple and valid index"
         (Project (Tuple [CstInt 1,CstInt 2,CstInt 3]) 2)
         (ValInt 3)
+      -- empty tuple projection
+      -- ().0 :: error
+      , evalTestFail
+        "Projection invalid index on empty tuple; any index is invalid"
+        (Project (Tuple []) 0)
       -- It is an error to try to project an element from a non-tuple
+      -- true.0 :: error
       , evalTestFail
         "Projection to a non-tuple"
         (Let "x" (CstBool True) (Project (Var "x") 0))
       -- If x has N elements, then i must be between 0 and N − 1; 
       -- using an index outside of this range is an error. 
+      -- (e0...en-1).n :: error
       , evalTestFail
         "Projection out-of-range"
-        (Let "x" (Tuple [CstInt 1,CstInt 2,CstInt 3,CstInt 4]) (Project (Var "x") 100))
+        (Let "x" (Tuple [CstInt 1,CstInt 2,CstInt 3,CstInt 4]) (Project (Var "x") 4))
 
       --
       -- Should work after Task B.
